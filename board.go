ef76f98e3bcd892a87b6bfec8d8c1f8c20f55880
Nv7 <junk4Nv7@gmail.com>
2020-12-06 20:17:24 -0800

    Changed to piece

--- board.go
+++ board.go
@@ -3,10 +3,22 @@ package main
 // Board contains the data for an othello board
 type Board struct {
 	Turn   bool
-	Pieces [][]bool // 8x8
+	Pieces [8][8]Piece // 8x8
 }
 
 // NewBoard initializes an Othello board
 func NewBoard() Board {
 	return Board{}
 }
+
+// Piece is a position on an Othello Board
+type Piece int
+
+const (
+	// Empty has an empty piece (*)
+	Empty Piece = 0
+	// Black is a black piece (B)
+	Black Piece = 1
+	// White is a white piece (W)
+	White Piece = 2
+)
