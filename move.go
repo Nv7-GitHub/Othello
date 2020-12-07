d0a16e02cf1241b4e0ae295fa81f6665fcd9f61b
Nv7 <junk4Nv7@gmail.com>
2020-12-07 11:09:20 -0800

    First version of move checking

--- move.go
+++ move.go
@@ -1,6 +1,22 @@
 package main
 
+type change struct {
+	row int
+	col int
+}
+
+var changes = []change{
+	change{row: 0, col: -1},
+	change{row: 1, col: 0},
+	change{row: 0, col: 1},
+	change{row: -1, col: 0},
+}
+
 // Move flips pieces as needed, provided with the piece that just got changed
 func (b *Board) Move(row, col int) {
-
+	for _, change := range changes {
+		if (b.Pieces[row-change.row][col-change.col] == Opposite(b.Pieces[row][col])) && (b.Pieces[row-change.row*2][col-change.col*2] == b.Pieces[row][col]) {
+			b.Pieces[row-change.row][col-change.col] = b.Pieces[row][col]
+		}
+	}
 }
